2e49629a0609fa77f41bb3b0423646901d742c38
hambu123 <hj012hj38317@gmail.com>
2020-07-01 19:24:59 +0900

    탄알피하기게임최초푸쉬

---
+++ dodge game/Assets/Dodge/Scripts/PlayerController.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public float m_Speed = 25f;
+    // Update is called once per frame
+    void Update()
+    {
+        Rigidbody rigidbody = /*gameObject.*/GetComponent<Rigidbody>();
+
+        float xAxis = Input.GetAxis("Horizontal");
+        float yAxis = Input.GetAxis("Vertical");
+        
+        rigidbody.AddForce(new Vector3(xAxis, 0, yAxis) * m_Speed);    
+
+        //Geaxis fire   사망처리 해보자.`
+        float fireAxis = Input.GetAxis("Firel");
+
+        if (fireAxis > 0.95f)
+            Die();
+    }
+
+
+    public void Die()
+    {
+        Debug.Log("사망");
+        gameObject.SetActive(false);
+    }
+}
---
+++ dodge game/Assets/test.cs
@@ -0,0 +1,24 @@
+﻿using UnityEditor;
+using UnityEngine;
+
+public class Test : EditorWindow
+{
+    // Start is called before the first frame update
+    [MenuItem("Window/Test")]
+
+    static void Init()
+    {
+        Test window = (Test)EditorWindow.GetWindow(typeof(Test));
+        window.Show();
+    }
+
+    // Update is called once per frame
+    void OnGUI()
+    {
+        Handles.color = Color.red;
+        Handles.DrawRectangle(0, new Vector3(200, 200, 0), Quaternion.identity, 100);
+        Handles.DrawSolidDisc(new Vector3(200, 200, 0), Vector3.forward, 100);
+        Handles.DrawSolidDisc(new Vector3(150, 150, 0), Vector3.forward, 50);
+        Handles.DrawSolidDisc(new Vector3(250, 150, 0), Vector3.forward, 50);
+    }
+}
