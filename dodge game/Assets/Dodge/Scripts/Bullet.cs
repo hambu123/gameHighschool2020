36a6684d6ace6ab6d12ab2016e4974100cc4022b
hambu123 <hj012hj38317@gmail.com>
2020-07-01 21:18:03 +0900

    수업시간

---
+++ dodge game/Assets/Dodge/Scripts/Bullet.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bullet : MonoBehaviour
+{
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.attachedRigidbody  != null && other.attachedRigidbody.tag == "Player")
+        {
+            var player = other.attachedRigidbody.GetComponent<PlayerController>();
+            player.Die();
+        }
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+    public float m_Speed = 5f;
+
+    public float m_DestroyCooltime = 5f;
+
+    // Update is called once per frame
+    void Update()
+    {
+       Rigidbody rigidbody = /*gameobject*/GetComponent<Rigidbody>();
+
+        rigidbody.AddForce(transform.forward * m_Speed);
+
+        m_DestroyCooltime -= Time.deltaTime;
+
+        if (m_DestroyCooltime <= 0)
+            gameObject.SetActive(false);
+       
+    }
+    
+   
+
+
+}
