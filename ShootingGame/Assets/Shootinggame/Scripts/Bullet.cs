03983758272f893fc62fc9c0b2c74b0c4aa01610
hambu123 <hj012hj38317@gmail.com>
2020-07-20 21:08:44 +0900

    1일차

---
+++ ShootingGame/Assets/Shootinggame/Scripts/Bullet.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bullet : MonoBehaviour
+{
+    public float m_Speed = 100f;
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 movement = transform.up * m_Speed * Time.deltaTime;
+        transform.position += movement;
+    }
+}
