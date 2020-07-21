b3b80729c65b8ecb2369e64e8127121bd3208944
hambu123 <hj012hj38317@gmail.com>
2020-07-21 21:25:19 +0900

    23123

---
+++ ShootingGame/Assets/Shootinggame/Scripts/EnemySmall.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemySmall : MonoBehaviour
+{
+    public float m_Speed = 3f;
+
+    public float m_LifeTime = 5f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+    
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position -= transform.up * m_Speed * Time.deltaTime;
+    }
+}
--- ShootingGame/Assets/Shootinggame/Scripts/PlayerControll.cs
+++ ShootingGame/Assets/Shootinggame/Scripts/PlayerControll.cs
@@ -4,30 +4,42 @@ using UnityEngine;
 
 public class PlayerControll : MonoBehaviour
 {
+    public float Speed = 3f;
 
-   
+    public Bullet m_Bulletfrefabs;
+    public float m_AttackDely = 0.5f;
+    public float m_AttackCooldown = 0f;
+
+    public Transform[] m_Firemuzzle;
+
+    
+
     private void Start()
     {
     
     }
     void Update()
     {
-      if(Input.GetKey(KeyCode.UpArrow))
-      {
-            transform.position += new Vector3(0, 3, 0) * Time.deltaTime;
-      }
-      if (Input.GetKey(KeyCode.LeftArrow))
-      {
-          transform.position += new Vector3(-3, 0, 0) * Time.deltaTime;
-      }
-      if (Input.GetKey(KeyCode.RightArrow))
-      {
-            transform.position += new Vector3(3, 0, 0) * Time.deltaTime;
-      }
-      if (Input.GetKey(KeyCode.DownArrow))
-      {
-            transform.position += new Vector3(0, -3, 0) * Time.deltaTime;
-      }
+        float xMove = Input.GetAxis("Horizontal") * Speed * Time.deltaTime;
+        float yMove = Input.GetAxis("Vertical") * Speed * Time.deltaTime;
+
+        this.transform.Translate(new Vector3(xMove, yMove, 0));
+
+        //총알발사
+        //Gameobject.instantie
+
+        if(Input.GetKey(KeyCode.Space)&& m_AttackCooldown <= 0)
+        {
+            foreach (var firemuzzle in m_Firemuzzle)
+            {
+                var bullet = GameObject.Instantiate(m_Bulletfrefabs, firemuzzle.position, firemuzzle.rotation);
+            }
+            //총알생성
+            
+            m_AttackCooldown = m_AttackDely;
+        }
+        m_AttackCooldown -= Time.deltaTime;
+        
     }
 }
   
