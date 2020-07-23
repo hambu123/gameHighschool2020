830b0470606db84b431ba2e06a55cb18e25a02d0
hambu123 <hj012hj38317@gmail.com>
2020-07-23 21:21:12 +0900

    12321

---
+++ AnimationTest/Assets/AnimationTest/Player.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    public Animator m_Animator;
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.F))
+        {
+            m_Animator.SetTrigger("Attack");
+        }
+    }
+}
