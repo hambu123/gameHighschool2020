9c2936bf5a642c93cbebb3aaa64980243488d389
hambu123 <hj012hj38317@gmail.com>
2020-09-07 21:26:27 +0900

    9/7

--- SunnyLand/Assets/GameSchool/Scripts/HpComponent.cs
+++ SunnyLand/Assets/GameSchool/Scripts/HpComponent.cs
@@ -1,18 +1,21 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Events;
 
 public class HpComponent : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public UnityEvent m_OnDie;
+    public UnityEvent m_OnTakeDamage;
+    public UnityEvent m_OnTakeHeal;
+
+    public virtual void TakeDamage(int damage)
     {
-        
+        m_OnTakeDamage.Invoke();
     }
 
-    // Update is called once per frame
-    void Update()
+    public void DestroySelf()
     {
-        
+        Destroy(gameObject);
     }
 }
